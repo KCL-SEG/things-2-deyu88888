d1a0208de9d4f6515d8f53f2cf01735381316734
Deyu Li <k21081622@kcl.ac.uk>
2023-12-08 11:30:36 +0000

    exercise 2

--- things/views.py
+++ things/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ThingForm
 
 def home(request):
-    form = ThingForm()
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
+
+
+    # if request.method == 'POST':
+    #     form = LogInForm(request.POST)
+    #     if form.is_valid():
+    #         username = form.cleaned_data.get('username')
+    #         password = form.cleaned_data.get('password')
+    #         user = authenticate(username=username, password=password)
+    #         if user is not None:
+    #             login(request, user)
+    #             return redirect('feed')
+    #     messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+    # form = LogInForm()
+    # return render(request, 'log_in.html', {'form': form})
